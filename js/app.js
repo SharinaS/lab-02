9b0f6bb48b6a8c63dc7516b7ff9fd3d311afbc1b
Sharina Stubbs <sharinastubbs@gmail.com>
2019-07-09 16:01:57 -0700

    added a counter to tally up correct answers

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
+var points = 0;
+
 // Question 1: Siblings
 var siblings = prompt('Do you think Sharina have siblings?');
 siblings = siblings.toLowerCase();
 if (siblings === 'yes' || siblings === 'y') {
   alert('Oooh, that\'s correct! Sharina has a sister!');
+  points += 1;
 } else {
   alert('Awww, nice try. Sharina actually does have siblings');
 }
@@ -15,8 +18,9 @@ var spaceNeedle = prompt('Do you think Sharina visited the top of the Space Need
 spaceNeedle = spaceNeedle.toLowerCase();
 if (spaceNeedle === 'no' || spaceNeedle === 'n') {
   alert('Great guess! You are absolutely correct: Sharina has never been to the top of the Space Needle.');
+  points += 1;
 } else {
-  alert('Awww, nice try. Sharina actually does have siblings');
+  alert('Weirdly enough, Sharina has actual never been to the top of the Space Needle. Keep trying!');
 }
 console.log('Answer 2: No, Sharina has not visited the top of the Space Needle.');
 
@@ -25,16 +29,18 @@ var visitAustralia = prompt('Question 3: Would you guess that Sharina has been t
 visitAustralia = visitAustralia.toLowerCase();
 if (visitAustralia === 'yes' || visitAustralia === 'y') {
   alert('Yes, is the correct answer! Sharina has been to Sydney, Australia... twice!');
+  points += 1;
 } else {
   alert('Sorry, that\'s not correct. Sharina actually has been to Australia!');
 }
 console.log('Answer 3: Yes, Sharina has been to Australia');
 
-// Question 4: Music Major 
+// Question 4: Music Major
 var musicMajor = prompt('Do you think Sharina was a music major in undergrad?');
 musicMajor = musicMajor.toLowerCase();
 if (musicMajor === 'yes' || musicMajor === 'y') {
   alert('Yes is correct! Sharina played string bass, was a composer, and ended up majoring in music');
+  points += 1;
 } else {
   alert('Whoops, that\'s not correct! Sharina really was a music major!');
 }
@@ -45,10 +51,17 @@ var caribbean = prompt('Do you think Sharina has lived in the Caribbean?');
 caribbean= caribbean.toLowerCase();
 if (caribbean === 'yes' || caribbean === 'y') {
   alert('Hey, good guessing skills! Sharina lived on an island that was 5 square miles, near St. Martin!');
+  points += 1;
 } else {
   alert('Oh sad, you missed that one. Sharina did call the Caribbean home for a while.');
 }
 console.log('Answer 5: Yes, Sharina lived in the Caribbean.');
 
-
-// When each yes is chosen, add 1 to a counter array. At end, check length of counter array and report points as document.write()
+var totalScore = document.getElementById('score');
+if (totalScore > 3) {
+  totalScore.textContent = 'Awesome job! You guessed ' + points + ' out of 5 questions correctly!';
+} else if (totalScore <= 1) {
+  totalScore.textContent = 'It was a valiant try. You guessed ' + points + ' out of 5 questions correctly.';
+} else {
+  totalScore.textContent = 'Not too shabby! You guessed ' + points + ' out of 5 questions correctly!';
+}
